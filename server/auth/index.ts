0a0b7dadf1eaeff355f86f00d70e426aba550e69
reterw <wsq530@hotmail.com>
2022-02-20 21:34:12 +0800

    entry point fix

--- server/auth/index.ts
+++ server/auth/index.ts
@@ -1,8 +1,8 @@
 import { defineHandler } from "../vote/utils";
 import axios from 'axios'
-export const CLIENT_SCERET = '6695f391a003c88418bd7a5d57c52e4137e4e2fb'
-export const CLIENT_ID = '100087b3bd25a77c425b'
-export const GITHUB_ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token'
+const CLIENT_SCERET = '6695f391a003c88418bd7a5d57c52e4137e4e2fb'
+const CLIENT_ID = '100087b3bd25a77c425b'
+const GITHUB_ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token'
 
 export default defineHandler(async (context, req) => {
     const authRes = await axios(`${GITHUB_ACCESS_TOKEN_URL}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SCERET}&code=${req.query.code}&state=${req.query.state}`, {
@@ -12,7 +12,7 @@ export default defineHandler(async (context, req) => {
     })
     const token = authRes.data.access_token
 
-    const userResp = await axios(`https://api.github.com/user`, { headers: { Authorization: `token ${token}` }})
+    const userResp = await axios(`https://api.github.com/user`, { headers: { Authorization: `token ${token}` } })
 
     return { userId: userResp.data.login, avatarUrl: userResp.data.avatar_url, accessToken: token }
 }, "GET")
