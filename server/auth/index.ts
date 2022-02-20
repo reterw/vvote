4329f486aa2ff48f420e4946a6340fc880726b03
reterw <wsq530@hotmail.com>
2022-02-20 21:29:17 +0800

    export deafult

--- server/auth/index.ts
+++ server/auth/index.ts
@@ -4,7 +4,7 @@ export const CLIENT_SCERET = '6695f391a003c88418bd7a5d57c52e4137e4e2fb'
 export const CLIENT_ID = '100087b3bd25a77c425b'
 export const GITHUB_ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token'
 
-export const auth = defineHandler(async (context, req) => {
+export default defineHandler(async (context, req) => {
     const authRes = await axios(`${GITHUB_ACCESS_TOKEN_URL}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SCERET}&code=${req.query.code}&state=${req.query.state}`, {
         headers: {
             Accept: 'application/json'
