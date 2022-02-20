479d9c5b31696ee7ff5ad45992722b599d5c0fb7
reterw <wsq530@hotmail.com>
2022-02-20 20:08:04 +0800

    update pipeline

--- src/constant.ts
+++ src/constant.ts
@@ -3,7 +3,6 @@ import { v4 } from "uuid"
 export const CLIENT_ID = '100087b3bd25a77c425b'
 export const CLIENT_SCERET = '6695f391a003c88418bd7a5d57c52e4137e4e2fb'
 export const REDIRECT_URL = import.meta.env.VITE_HOST_URL
-// export const REDIRECT_URL = 'http://localhost:3000'
 
 export const AUTH_SERVER_URL = 'https://602vote.azurewebsites.net/api/auth'
 export const GITHUB_LOGIN_URL = 'https://github.com/login/oauth/authorize'
