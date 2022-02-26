60a40e5d3c510854777506c230bebb213b965587
reterw <wsq530@hotmail.com>
2022-02-26 12:35:13 +0800

    about app

--- src/about.vue
+++ src/about.vue
@@ -23,6 +23,45 @@
                     href="https://602vote.azurewebsites.net/api/vote"
                 >https://602vote.azurewebsites.net/api/vote</a>
                 <img src="./image/602/RestEndpoints.jpg" />
+
+                <label class="text-4xl">Log in with github Oauth</label>
+                <br />
+                <br />
+                <img src="./image/602/githuboauth.jpg" />
+                
+                <label class="text-4xl">Home Page render all user's vote together, and the vote box does not have delete button</label>
+                <br />
+                <br />
+                <img src="./image/602/homepage.jpg" />
+
+                <label class="text-4xl">my page is for admin, after you login , you can render the vote create by you, and also delete them by click the button.</label>
+                <br />
+                <br />
+                <img src="./image/602/mypage.jpg" />
+               
+                <label class="text-4xl">for create vote, enter the title of the vote problem, and enter choice of it, then click submit.</label>
+                <br />
+                <br />
+                <img src="./image/602/createvote.jpg" />
+                <img src="./image/602/createvote2.jpg" />
+
+                <label class="text-4xl">click thumb to vote your choice.</label>
+                <br />
+                <br />
+                <img src="./image/602/vote1.jpg" />
+                <img src="./image/602/vote2.jpg" />
+
+                <label class="text-4xl">vote count will change in data base, and each prople's vote are recorded.</label>
+                <br />
+                <br />
+                <img src="./image/602/votecount.jpg" />
+                <img src="./image/602/votehistory.jpg" />
+
+                <label class="text-4xl">using delete, get, post and put methods.</label>
+                <br />
+                <br />
+                <img src="./image/602/restapi.jpg" />
+
             </div>
         </div>
     </div>
